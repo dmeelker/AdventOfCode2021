f6bbb944ee3db8b75ca0a336c45520ea447426a9
Dennis Meelker <dennis.meelker@infi.nl>
2021-12-14 20:44:30 +0100

    Day 14

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -8,16 +8,22 @@ struct Point {
     y: i32,
 }
 
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point {x, y}
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Fold {
     X(i32),
-    Y(i32)
+    Y(i32),
 }
 
 #[derive(Debug)]
 struct Input {
     points: Vec<Point>,
-    folds: Vec<Fold>
+    folds: Vec<Fold>,
 }
 
 fn main() {
@@ -32,79 +38,80 @@ fn main() {
 }
 
 fn parse_input(input: &str) -> Input {
-    let parts = input.split("\r\n\r\n").collect_vec();
+    let parts = input.split_once("\r\n\r\n").unwrap();
 
     Input { 
-        points: parse_points(parts[0]),
-        folds: parse_folds(parts[1]),
+        points: parse_points(parts.0),
+        folds: parse_folds(parts.1),
     }
 }
 
 fn parse_points(input: &str) -> Vec<Point> {
-    input.lines().map(parse_point).collect_vec()
-}
-
-fn parse_point(input: &str) -> Point {
-    let values: (&str, &str) = input.split(',').collect_tuple().unwrap();
-    Point { 
-        x: values.0.parse().unwrap(),
-        y: values.1.parse().unwrap()
-    }
+    input.lines().map(|line| {
+        let values = line.split_once(',').unwrap();
+        Point::new(
+            values.0.parse().unwrap(),
+            values.1.parse().unwrap()
+        )
+    }).collect_vec()
 }
 
 fn parse_folds(input: &str) -> Vec<Fold> {
-    input.lines().map(parse_fold).collect_vec()
-}
-
-fn parse_fold(input: &str) -> Fold {
-    let values: (&str, &str) = input[11..].split('=').collect_tuple().unwrap();
-    let value: i32 = values.1.parse().unwrap();
-
-    match values.0 {
-        "y" => Fold::Y(value),
-        "x" => Fold::X(value),
-        _ => panic!()
-    }
+    input.lines().map(|line| {
+        let values = line[11..].split_once('=').unwrap();
+        let value: i32 = values.1.parse().unwrap();
+    
+        match values.0 {
+            "y" => Fold::Y(value),
+            "x" => Fold::X(value),
+            _ => panic!()
+        }
+    }).collect_vec()
 }
 
 fn fold(points: &HashSet<Point>, fold: Fold) -> HashSet<Point> {
-    if let Fold::X(x) = fold {
-        let left = points.iter().filter(|p| p.x < x).map(|p| *p);
-        let right = points.iter().filter(|p| p.x > x).map(|p| Point{ x: x - (p.x - x), y: p.y});
-        return left.chain(right).collect();
-    } if let Fold::Y(y) = fold {
-        let top = points.iter().filter(|p| p.y < y).map(|p| *p);
-        let bottom = points.iter().filter(|p| p.y > y).map(|p| Point{ x: p.x, y: y - (p.y - y)});
-        return top.chain(bottom).collect();
-    } else {
-        panic!();
+    match fold {
+        Fold::X(fold_x) => {
+            let left = points.iter().filter(|p| p.x < fold_x).copied();
+            let right = points.iter().filter(|p| p.x > fold_x).map(|p| Point::new(fold_coordinate(p.x, fold_x), p.y));
+            left.chain(right).collect()
+        },
+        Fold::Y(fold_y) => {
+            let top = points.iter().filter(|p| p.y < fold_y).copied();
+            let bottom = points.iter().filter(|p| p.y > fold_y).map(|p| Point::new(p.x, fold_coordinate(p.y, fold_y)));
+            top.chain(bottom).collect()
+        }
     }
 }
 
+fn fold_coordinate(coordinate: i32, fold_location: i32) -> i32 {
+    fold_location - (coordinate - fold_location)
+}
+
 fn part1(input: &Input) -> usize {
-    let mut points:  HashSet<Point> = input.points.iter().map(|p| *p).collect();
+    let mut points:  HashSet<Point> = input.points.iter().copied().collect();
     points = fold(&points, input.folds[0]);
     points.len()
 }
 
 fn part2(input: &Input) -> String {
-    let mut points:  HashSet<Point> = input.points.iter().map(|p| *p).collect();
-    for f in input.folds.iter() {
-        points = fold(&points, *f);
-    }
+    let points = input.folds.iter()
+        .fold(
+            input.points.iter().copied().collect(),
+            |points, f| fold(&points, *f)
+        );
     
     render_points(&points)
 }
 
 fn render_points(points: &HashSet<Point>) -> String {
     let mut chars: Vec<char> = Vec::new();
+    let max_x = points.iter().map(|p| p.x).max().unwrap();
+    let max_y = points.iter().map(|p| p.y).max().unwrap();
 
-    let width = points.iter().map(|p| p.x).max().unwrap() + 1;
-    let height = points.iter().map(|p| p.y).max().unwrap() + 1;
-
-    for y in 0..height {
-        for x in 0..width {
-            let point = points.contains(&Point {x: x as i32, y: y as i32});
+    for y in 0..=max_y {
+        for x in 0..=max_x {
+            let point = points.contains(&Point::new(x as i32, y as i32));
 
             if point {
                 chars.push('#');
