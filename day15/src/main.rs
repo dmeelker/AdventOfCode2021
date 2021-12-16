dfef879838e11c7e490052fef10051c1aeefa219
Dennis Meelker <dennis.meelker@infi.nl>
2021-12-16 10:58:44 +0100

    Cleanup

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,9 +1,6 @@
-use core::fmt;
-use std::{fs, collections::{HashSet, HashMap}, slice::SliceIndex};
+use std::{fs, collections::{HashSet, HashMap}};
 use itertools::Itertools;
 use priority_queue::PriorityQueue;
-
-//type Grid = Vec<Vec<i32>>;
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct Point {
@@ -68,31 +65,6 @@ impl Grid {
         }
     }
 
-    fn copy_block(&self, target: &mut Grid, source_location: &Point, target_location: &Point, width: usize, height: usize) {
-        for x in source_location.x..source_location.x + width as i32 {
-            for y in source_location.y..source_location.y + height as i32 {
-                let target_location  = Point::new(target_location.x + (x - source_location.x), target_location.y + (y - source_location.y));
-
-                target.set(&target_location, self.get(&Point::new(x, y)).unwrap());
-            }    
-        }
-    }
-
-    fn increment_block(&mut self, location: &Point, width: usize, height: usize) {
-        for x in location.x..location.x + width as i32 {
-            for y in location.y..location.y + height as i32 {
-                let location  = Point::new(x, y);
-                let mut new_value = self.get(&location).unwrap() + 1;
-
-                if new_value > 9 {
-                    new_value = 1;
-                }
-
-                self.set(&location, new_value);
-            }    
-        }
-    }
-
     fn get_all_points(&self) -> Vec<Point> {
         let mut points = Vec::new();
 
@@ -147,47 +119,18 @@ fn part2(grid: &Grid) -> i32 {
 
 fn prepare_part2_grid(input: &Grid) -> Grid {
     let mut grid = Grid::new(input.width*5, input.height*5, 0);
-    let mut data = Vec::new();
 
-    for _ in 0..input.height * 5 {
-        let mut column = Vec::new();
-        column.resize(input.width*5, 0);
-        data.push(column);
-    }
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let mut value = input.get(&Point::new((x % input.width) as i32, (y % input.height) as i32)).unwrap() - 1;
+            let increment = ((x / input.width) + (y / input.height)) as i32;
+            value = ((value + increment) % 9) + 1;
 
-    for y in 0..5 {
-        for x in 0..5 {
-            input.copy_block(&mut grid, &Point::new(0, 0), &Point::new(x * input.width as i32, y * input.height as i32), input.width, input.height);
-            // let cost_increase = (x / input.width) as i32; 
-            // data[y][x] = ((input.get(&Point {x: (x % input.width) as i32, y: y as i32}).unwrap() - 1 + cost_increase) % 9);
+            grid.set(&Point::new(x as i32, y as i32), value);
         }
     }
 
-    
-
-    grid.print();
-
-    for y in 0..input.height {
-        for x in 0..input.width * 5 {
-            let cost_increase = (x / input.width) as i32; 
-            data[y][x] = ((input.get(&Point {x: (x % input.width) as i32, y: y as i32}).unwrap() - 1 + cost_increase) % 9);
-        }
-    }
-
-    for y in input.height..input.height*5 {
-        for x in 0..input.width * 5 {
-            let cost_increase = (y / input.height) as i32;
-            data[y][x] = (data[y % input.height][x] + cost_increase) % 9;
-        }
-    }
-
-    for y in 0..input.height*5 {
-        for x in 0..input.width * 5 {
-            data[y][x] += 1;
-        }
-    }
-
-    Grid::from_data(data)
+    grid
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
@@ -198,55 +141,61 @@ struct Node {
     path_source: Option<Point>
 }
 
+impl Node {
+    fn from_point(point: &Point, cost: i32, is_start: bool) -> Node {
+        Node 
+        {
+            location: *point,
+            cost,
+            path_cost: if is_start { 0 } else {i32::MAX},
+            path_source: None
+        }
+    }
+}
+
 fn find_best_path(grid: &Grid, start: &Point, end: &Point) -> Vec<Point> {
-    let mut nodes: HashMap<Point, Node> = grid.get_all_points().iter().map(|p| 
+    let mut nodes: HashMap<Point, Node> = grid.get_all_points().iter().map(|point| 
         (
-            *p,
-            Node 
-            {
-                location: *p,
-                cost: grid.get(p).unwrap(),
-                path_cost: i32::MAX,
-                path_source: None
-            }
+            *point,
+            Node::from_point(point, grid.get(point).unwrap(), point == start)
         )).collect();
 
-    let mut unvisited: HashSet<Point> = nodes.keys().cloned().collect();
+    let mut unvisited_set: HashSet<Point> = nodes.keys().cloned().collect();
     let mut unvisited_queue: PriorityQueue<Point, i32> = PriorityQueue::new();
     
-    for p in unvisited.iter() {
-        unvisited_queue.push(*p, 0);
+    for p in unvisited_set.iter() {
+        unvisited_queue.push(*p,  0);
     }
 
-    nodes.entry(*start).and_modify(|node| node.path_cost = 0);
-
     let mut current_location = *start;
-    
-    loop {
+
+    while current_location != *end {
         let neighbours = get_neighbours(&current_location);
-        let neighbours = neighbours.iter().filter(|p| unvisited.contains(*p)).collect_vec();
         let current_node = nodes.get(&current_location).unwrap().clone();
 
         for neighbour in neighbours.iter() {
+            if !unvisited_set.contains(neighbour) {
+                continue;
+            }
+
             let neighbour_node = nodes.get_mut(*&neighbour).unwrap();
-            let cost = current_node.path_cost + neighbour_node.cost;
+            let cost_via_current_node = current_node.path_cost + neighbour_node.cost;
             
-            if cost < neighbour_node.path_cost {
-                neighbour_node.path_cost = cost;
+            if cost_via_current_node < neighbour_node.path_cost {
+                neighbour_node.path_cost = cost_via_current_node;
                 neighbour_node.path_source = Some(current_location);
-                unvisited_queue.push_increase(**neighbour, i32::MAX - neighbour_node.path_cost);
+                unvisited_queue.push_increase(*neighbour, i32::MAX - neighbour_node.path_cost);
             }
         }
 
-        unvisited.remove(&current_location);
-
-        if current_location == *end {
-            break;
-        }
-
+        unvisited_set.remove(&current_location);
         current_location = unvisited_queue.pop().unwrap().0;
     }
 
+    get_path(nodes, end, start).iter().map(|p| *p).rev().collect()
+}
+
+fn get_path(nodes: HashMap<Point, Node>, end: &Point, start: &Point) -> Vec<Point> {
     let mut path = Vec::new();
     let mut node = nodes.get(end).unwrap();
 
@@ -257,10 +206,9 @@ fn find_best_path(grid: &Grid, start: &Point, end: &Point) -> Vec<Point> {
             _ => panic!()
         }
     }
-
     path.push(*start);
 
-    path.iter().map(|p| *p).rev().collect()
+    path
 }
 
 fn get_neighbours(center: &Point) -> Vec<Point> {
